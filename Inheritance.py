804ec28a929a114d9b9e088e2c0d9442b0cdc308
Mainada109 <136803415+Mainada109@users.noreply.github.com>
2023-07-16 18:44:51 +0300

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,44 @@
+#Simple Inheritance
+
+class A:
+    pass
+class B(A):
+    pass
+
+#Multi Inheritance
+
+class A:
+    pass
+class B(A):
+    pass
+class C(B):
+    pass
+
+#Multiple Inheritance
+
+class A:
+    pass
+class B:
+    pass
+class C(A,B):
+    pass
+
+#Hierarchial Inheritance
+
+class A:
+    pass
+class B(A):
+    pass
+class C(A):
+    pass
+
+#Hybrid Inheritance
+
+class A:
+    pass
+class B(A):
+    pass
+class C(A):
+    pass
+class D(B,C):
+    pass
